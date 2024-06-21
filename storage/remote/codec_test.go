76999bbdf58d784c47279e1f2edac96defb6ad54
Matthieu MOREL <matthieu.morel35@gmail.com>
2024-06-21 11:53:14 +0200

    Update codec_test.go
    Signed-off-by: Matthieu MOREL <matthieu.morel35@gmail.com>

--- storage/remote/codec_test.go
+++ storage/remote/codec_test.go
@@ -225,19 +225,19 @@ func TestConcreteSeriesIterator_FloatSamples(t *testing.T) {
 	require.Equal(t, chunkenc.ValFloat, it.Seek(1))
 	ts, v := it.At()
 	require.Equal(t, int64(1), ts)
-	require.Equal(t, 1., v)
+	require.InDelta(t, 1., v, 0.01)
 
 	// Seek one further, next sample still has ts=1.
 	require.Equal(t, chunkenc.ValFloat, it.Next())
 	ts, v = it.At()
 	require.Equal(t, int64(1), ts)
-	require.Equal(t, 1.5, v)
+	require.InDelta(t, 1.5, v, 0.01)
 
 	// Seek again to 1 and make sure we stay where we are.
 	require.Equal(t, chunkenc.ValFloat, it.Seek(1))
 	ts, v = it.At()
 	require.Equal(t, int64(1), ts)
-	require.Equal(t, 1.5, v)
+	require.InDelta(t, 1.5, v, 0.01)
 
 	// Another seek.
 	require.Equal(t, chunkenc.ValFloat, it.Seek(3))
